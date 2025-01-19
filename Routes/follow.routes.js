fb44853ff004b1702faba46c0b799173215e305f
unknown <sreerajm089@gmail.com>
2025-01-19 12:36:30 +0530

    Updated backend code to include Firebase config from environment variables

--- Routes/follow.routes.js
+++ Routes/follow.routes.js
@@ -3,14 +3,13 @@ const User = require('../models/user.model.js');
 const router = express.Router();
 const Notification = require('../models/notification.model.js');
 const admin = require('firebase-admin');
-const serviceAccount = require('../../myapp-1f7eb-firebase-adminsdk-m8kjj-54e2d43897.json');
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
-// Initialize Firebase Admin SDK 
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        projectId: 'myapp-1f7eb',
+        projectId: serviceAccount.project_id,
     });
 }
 
